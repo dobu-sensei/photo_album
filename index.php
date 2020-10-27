584bd083a3e7f116b4b7db8bf29fae08276ed457
hnakashima <nakashima@xio.jp>
2020-10-27 20:42:08 +0900

    HTML②HTMLタグ6種おぼえて10分でページ構成

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>PHOTO ALBUM</title>
+    </head>
+    <body>
+        <div>
+            <h1>PHOTO ALBUM</h1>
+            <div>
+                <ul>
+                    <li><a href="#">新規投稿</a></li>
+                    <li><a href="#">マイページ</a></li>
+                    <li><a href="#">ログアウト</a></li>
+                </ul>
+            </div>
+        <div>
+        <div>
+            <div>
+                <div>
+                    名前
+                </div>
+                <img src="https://picsum.photos/300/300/">
+                <p>メッセージ</p>
+            </div>
+            <div>
+                <div>
+                    名前
+                </div>
+                <img src="https://picsum.photos/300/300/">
+                <p>メッセージ</p>
+            </div>
+            <div>
+                <div>
+                    名前
+                </div>
+                <img src="https://picsum.photos/300/300/">
+                <p>メッセージ</p>
+            </div>
+        </div>
+        <div>
+            <small>@dobu sensei 2020</small>
+        </div>
+    </body>
+</html>
